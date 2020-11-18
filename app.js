55eff8a958f0b37770dff68f5d0cbc7b3cec6186
rodmarrec <romarron01@gmail.com>
2020-11-18 13:15:51 -0600

    adds oAth2Client functions on app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // STUB External modules
-// tell our app to require the express and request modules
+// tell our app to require the modules we've installed
 const express = require("express");
 const request = require("express");
+const fs = require("fs");
+const readline = require("readline");
+const {google} = require("googleapis");
 
 // STUB Instanced modules
 // we create a new express instance
@@ -13,20 +16,122 @@ app.set("view engine", "ejs");
 
 // STUB middleware
 // tell express to serve static files from the  "Public" directory 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 // STUB Routes
 
 // view routes
 app.get("/", function(req, res) {
-    console.log("Hello!");
+    // load client from a local file
+    fs.readFile("credentials.json", (err, content) => {
+        if(err)
+            return console.log("Error loading client secret file:", + err);
+
+    // authorize a client with credentials, then call the Google Sheets API
+    authorize(JSON.parse(content), listMajors);
+    });
+
+    /** ANCHOR creates OAuth2 param
+     * Prints the names and majors of students in a sample spreadsheet
+     * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+     * @param {google.auth.OAuth2} auth The authenticated Google OAuth client
+     */
+
+    function listMajors(auth) {
+        const sheets = google.sheet({version: "v4", auth});
+        sheets.spreadsheets.values.get({
+            spreadsheetId: "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms",
+            range: "Class Data!A2:E",
+        }, (err, res) => {
+            if(err) 
+                return console.log("The API returned an error:", + err);
+
+            const rows = res.data.values;
+            if (rows.length) {
+                console.log("Name, Major:");
+
+            // print columns A and E, which correspond to indices 0 and 4
+            rows.map((row) => {
+                console.log(`${row[0]}, ${row[4]}`);
+            });
+            } else {
+                console.log("No data found");
+            }
+        });
+    }
 })
 
 
+// STUB Scopes
+// if modifying these scopes, delete token.json
 
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 
+// the file token.json stores the user's access and refresh tokens, and is created automatically when authorization flow completes for the first time
 
+const TOKEN_PATH = "token.json";
 
+/** STUB create instance of OAuth2 client
+ * Create an OAuth2 client with the given credentials, and then execute the given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client
+ */
+
+function authorize(credentials, callback) {
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+    // check if we have previously stored a token
+    fs.readFile(TOKEN_PATH, (err, token) => {
+        if(err)
+            return getNewToken(oAuth2Client, callback);
+        
+            oAuth2Client.setCredentials(JSON.parse(token));
+
+            callback(oAuth2Client);
+    });
+}
+
+/** STUB get and store new token
+ * Get and store new token after prompting for user authorization, and then execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client
+ */
+
+function getNewToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: "offline",
+        scope: SCOPES,
+    });
+
+    console.log("Authorize this app by visiting this url:", authUrl);
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    rl.question("Enter the code from that page here: ", (code) => {
+        rl.close();
+
+        oAuth2Client.getToken(code, (err, token) => {
+            if(err)
+                return console.error("Error while trying to retrieve access token:", + err);
+
+            oAuth2Client.setCredentials(token);
+
+            // ANCHOR store the token to disk for later program executions
+            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                if (err)
+                    return console.error(err);
+                    console.log("Token stored to:", TOKEN_PATH);
+            });
+
+            callback(oAuth2Client);
+        });
+    });
+}
 
 // Server Listener
 // process.env.PORT will let our environment (ex: Heroku) set the variable for us later
